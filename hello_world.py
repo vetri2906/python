ab68e27d7984911b205f6e584ba0a8fa1c63ade1
vetri <vetri290696@gmail.com>
2024-09-21 00:52:51 +0530

    basics

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+print("hello world check")
+print() # similar to \n
+# if opened in single quotes - message inside should have double quotes
+#nested single quotes or nested double quotes wont work
+print('print("what to print")') 
+print ('hello' + " " 'world')
+#print() works in 3.x of python only and it is a function not a statement
+#In earlier versions of python (python 2.x) print is a statement, like : print "hello world" this will throw error in python 3.x versions
---
+++ input_fn.py
@@ -0,0 +1,1 @@
+print("hii" + " " +input("what is your name ? \n") + ", " + "how are you ?")
